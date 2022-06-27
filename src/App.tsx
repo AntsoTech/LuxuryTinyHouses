549a7457723489f7bd0ab390b683d087d7a88308
AntsoTech <antso-rakotoson@hotmail.fr>
2022-06-27 11:09:49 +0200

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import './App.scss';
 import React from 'react';
 import Concept from './components/Concept';
-import Contact from './components/Contact';
 import Header from './components/Header';
-import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Story from './components/Story';
 import Values from './components/values';
@@ -11,13 +9,11 @@ import Values from './components/values';
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Header />
       <Concept />
       <Projects />
       <Values />
       <Story />
-      <Contact />
     </div>
   );
 }
